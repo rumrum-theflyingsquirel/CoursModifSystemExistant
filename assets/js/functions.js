0807dbf040d9257d64fd7e6cdaaa3b805cd71219
Francis <ftrudeau@gmail.com>
2022-02-08 21:05:01 -0500

    Projet Apollo

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -15,9 +15,9 @@ function ScrollingEffect(){
 	$('a[href^="#"]').click(function(scroll){
 	scroll.preventDefault();
 
-	var target = $($(this).attr('href'));
+	var section = $($(this).attr('href'));
 	
-	$('html, body').animate({ 'scrollTop': target.offset().top - 170 }, 1500, function(){
+	$('html, body').animate({ 'scrollTop': section.offset().top - 170 }, 1500, function(){
 	});
 
 	$(".nav").toggleClass("open");
@@ -28,36 +28,42 @@ function ScrollingEffect(){
 function Carousel(){
 	$('#full-slide .prev, #full-slide .next').click(function(){		
 		var $this = $(this),
-		 		actif = $('.banner').find('.active'),
-		 		position = $('.banner').children().index(actif),
-				nbimg = $('.banner').children().length;				
-		 if ($this.hasClass('next')){			
-		 	if (position < nbimg - 1){
-		 		$('.active').removeClass('active').next().addClass('active');
-		 	}			
-		 	else {	
-		 		$('.banner li').removeClass('active').first().addClass('active');
-		 	}			
-		 }		
-		 else {			
-			if (position === 0){
-				$('.banner li').removeClass('active').last().addClass('active');
-		 	}			
-		 	else {
-		 		$('.active').removeClass('active').prev().addClass('active');
-		 	}		
-		 }	
-	});
+		 		imgactive = $('.banner').find('.active'),
+		 		imgposition = $('.banner').children().index(imgactive),
+				nbimg = $('.banner').children().length;		
+				
+				
+	if ($this.hasClass('next')){			
+		if (imgposition < nbimg - 1){
+		 	$('.active').removeClass('active').next().addClass('active');
+		}			
+		else {	
+		 	$('.banner li').removeClass('active').first().addClass('active');
+		}			
+	}		
+		 
+	else {			
+		if (position === 0){
+			$('.banner li').removeClass('active').last().addClass('active');
+		}			
+		else {
+		 	$('.active').removeClass('active').prev().addClass('active');
+		}		
+	}	
+});
 }
 
+
+
+
 function Sendemail(){
+
 
 	$('.SendEmail').click(function(){	
 		var Email = document.getElementById("email").value;
 		var Sujet = document.getElementById("sujet").value;
 		var Message = document.getElementById("message").value;
-
-		window.location.href = "mailto:" + Email + "?subject=" + Sujet + "&body=" + Message ;
+				window.location.href = "mailto:Receveur@ducourriel.com" + "?subject=" + Sujet + "&body=" + Message +"     from :"+ Email  ;
 		
 	});
 }
