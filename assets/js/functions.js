a7cc3d6a1f33f4d5e94357d23533e5947bddeb18
Francis <ftrudeau@gmail.com>
2022-02-08 20:39:04 -0500

    Projet Apollo

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,76 +1,75 @@
 $( document ).ready(function() {
+
 	
-function nav(){
-	
-	$('.nav-toggle').click(function(){
-		
+
+
+function Menu(){
+	$('.nav-toggle').click(function(){	
 		$('.nav').toggleClass('open');
+	});	
+}
+
+
+
+function ScrollingEffect(){
+	$('a[href^="#"]').click(function(scroll){
+	scroll.preventDefault();
+
+	var target = $($(this).attr('href'));
+	
+	$('html, body').animate({ 'scrollTop': target.offset().top - 170 }, 1500, function(){
+	});
+
+	$(".nav").toggleClass("open");
+	});
+}
+
+
+function Carousel(){
+	$('#full-slide .prev, #full-slide .next').click(function(){		
+		var $this = $(this),
+		 		actif = $('.banner').find('.active'),
+		 		position = $('.banner').children().index(actif),
+				nbimg = $('.banner').children().length;				
+		 if ($this.hasClass('next')){			
+		 	if (position < nbimg - 1){
+		 		$('.active').removeClass('active').next().addClass('active');
+		 	}			
+		 	else {	
+		 		$('.banner li').removeClass('active').first().addClass('active');
+		 	}			
+		 }		
+		 else {			
+			if (position === 0){
+				$('.banner li').removeClass('active').last().addClass('active');
+		 	}			
+		 	else {
+		 		$('.active').removeClass('active').prev().addClass('active');
+		 	}		
+		 }	
+	});
+}
+
+function Sendemail(){
+
+	$('.SendEmail').click(function(){	
+		var Email = document.getElementById("email").value;
+		var Sujet = document.getElementById("sujet").value;
+		var Message = document.getElementById("message").value;
+
+		window.location.href = "mailto:" + Email + "?subject=" + Sujet + "&body=" + Message ;
 		
 	});
-	
-}
-
-function smoothScroll(){
-	// FAIRE
-	// $('a[href^="#"]').click(function(event){
-		
-	// 	var target = $($(this).attr('href'));
-
-    // if (target.length){
-    //   event.preventDefault();
-    //   $('html, body').animate({
-	//       scrollTop: target.offset().top - 15
-    //   }, 300);
-    // }
-		
-	// 	$('.nav').toggleClass('open');
-		
-	// });
-	
-}
-
-function fullSlider(){
- // A FAIRE
-	//$('#full-slide .prev, #full-slide .next').click(function(){
-		
-		// var $this = $(this),
-		// 		current = $('.banner').find('.active'),
-		// 		position = $('.banner').children().index(current),
-		// 		totalPics = $('.banner').children().length;
-				
-		// if ($this.hasClass('next')){
-			
-		// 	if (position < totalPics - 1){
-		// 		$('.active').removeClass('active').next().addClass('active');
-		// 	}
-			
-		// 	else {	
-		// 		$('.banner li').removeClass('active').first().addClass('active');
-		// 	}
-			
-		// }
-		
-		// else {
-			
-		// 	if (position === 0){
-		// 		$('.banner li').removeClass('active').last().addClass('active');
-		// 	}
-			
-		// 	else {
-		// 		$('.active').removeClass('active').prev().addClass('active');
-		// 	}
-			
-		// }
-		
-	//});
-
 }
 
 
-nav();
 
-smoothScroll();
 
-fullSlider();
 
+
+
+Menu();
+ScrollingEffect();
+Carousel();
+Sendemail();
 });
